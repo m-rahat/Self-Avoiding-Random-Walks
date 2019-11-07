a7774e2e0531e4d4a8ae1698ef135d555e98cd77
m-rahat <56198714+m-rahat@users.noreply.github.com>
2019-11-07 17:25:17 -0500

    Holds global variables

---
+++ Globals.java
@@ -0,0 +1,15 @@
+
+public final class Globals {
+
+	static int Number_of_Steps = 2;
+
+	static int dimensions = 4;
+
+	static int Number_of_Threads = 1000;
+
+	static int max_steps = 40;
+
+	// 10^5
+	static int Number_of_Walks = 100000;
+
+}// Globals
